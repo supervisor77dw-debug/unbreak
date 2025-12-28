c197b54e6404f2f2254e9e28f8a304706609ca4e
Dirk Wilkens <supervisor77.dw@googlemail.com>
2025-12-28 15:24:39 +0100

    Add pages/index.js to fix root route 404

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+﻿import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    // Redirect to index.html (served from public/)
+    window.location.href = '/index.html';
+  }, []);
+
+  return null;
+}
